c4b4f7fe6678c8a511c8e53b298cdaab7f6c9ac4
MFQuraishi <qmohammadfaiz@gmail.com>
2023-08-12 01:54:32 +0530

    base setup for form

--- src/components/HeroBanner/HeroBanner.tsx
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -1,9 +1,12 @@
 import "./HeroBanner.scss"
+import HeroRegisterForm from "./../UserRegisterForm/UserRegisterForm"
 
 export default function HeroBanner() {
     return (<div className="hero-banner-container">
         <div className="hero-banner-top-layer">
-            
+            <div className="hero-banner-form-container">
+                <HeroRegisterForm/>
+            </div>
         </div>
     </div>)
 } 
---
+++ src/components/UserRegisterForm/UserRegisterForm.tsx
@@ -0,0 +1,12 @@
+import "./UserRegisterForm.scss"
+
+export default function UserRegisterForm() {
+    return (
+        <div className="form-base-plate">
+            <select className="">
+                <option value="someOption">Some option</option>
+                <option value="otherOption">Other option</option>
+            </select>
+        </div>
+    );
+}
