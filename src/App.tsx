8408aeb7b2693a9ccf218b0792976f66b5b1f87f
MFQuraishi <qmohammadfaiz@gmail.com>
2023-08-12 01:27:55 +0530

    setting up hero banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import HeroBanner from './components/HeroBanner/HeroBanner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='demo-top-bar'>
+        Navbar goes here
+      </div>
+      <HeroBanner/>
+      <div className='demo-content'>
+        Page Content goes here
+      </div>
+    </>);
 }
 
 export default App;
---
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -0,0 +1,9 @@
+import "./HeroBanner.scss"
+
+export default function HeroBanner() {
+    return (<div className="hero-banner-container">
+        <div className="hero-banner-top-layer">
+            
+        </div>
+    </div>)
+} 
